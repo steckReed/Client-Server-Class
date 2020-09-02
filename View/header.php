4cf29a4bcb074e191e2b8eb49cccd192331b46b6
steckReed <61429429+steckReed@users.noreply.github.com>
2020-09-02 12:37:31 -0400

    Template - First Push of Homework

---
+++ View/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Meta Tags -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- /Meta Tags -->
+
+  <!-- Links -->
+        <link rel='stylesheet' href="./View/Public/CSS/custom-styles.css">
+  <!-- /Links -->
+  <title>Gamer Goop</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    include './View/header.php';
+    // include './View/navbar.php';
+    // include './View/Guru-1/game-of-the-week.html';
+    // include './View/Guru-2/game-of-the-week.html';
+?>
+
+<div class="parent">
+  <div class="image-container-left"></div>
+  <div class="image-container-right"></div>
+</div>
+
+
+
+<?php
+    include './View/footer.php';
+?>
